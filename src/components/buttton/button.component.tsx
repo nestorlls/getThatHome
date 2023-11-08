fa6eff43c09a0dd514a5dc18ec5666e5b642e36e
Paul Llanque <llanquepol@gmail.com>
2023-11-08 17:37:08 -0500

    [UPDATE] Button icon update

--- src/components/buttton/button.component.tsx
+++ src/components/buttton/button.component.tsx
@@ -4,17 +4,23 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   typeBtn?: 'primary' | 'secondary' | 'subtle';
   size?: 'small' | 'medium' | 'large';
   children: React.ReactNode;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
 }
 
 export const Button: React.FC<ButtonProps> = ({
   children,
   typeBtn = 'primary',
   size = 'medium',
+  leftIcon,
+  rightIcon,
   ...props
 }) => {
   return (
     <button className={`btn ${typeBtn} ${size}`} {...props}>
+      {leftIcon ? <span>{leftIcon}</span> : null}
       {children}
+      {rightIcon ? <span>{rightIcon}</span> : null}
     </button>
   );
 };
