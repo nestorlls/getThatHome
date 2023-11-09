068b5a4748b8c7cce8e9be75ddedf03b6c09ac76
Paul Llanque <llanquepol@gmail.com>
2023-11-08 19:55:11 -0500

    [REFACTOR] reafactor components

---
+++ src/components/baseComponents/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './buttton/button.component';
+export { FormikInput } from './input/input.component';
+export { FormikTextArea } from './textArea/textarea.component';
+export { CheckBox } from './checkbox/checkbox.component';
+export { NumberButton } from './numberButton/numberButton.component';
--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import { Button } from './buttton/button.component';
-import { FormikInput } from './input/input.component';
-import { FormikTextArea } from './textArea/textarea.component';
-import { CheckBox } from './checkbox/checkbox.component';
-
-export { Button, CheckBox, FormikInput, FormikTextArea };
