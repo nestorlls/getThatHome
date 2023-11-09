5158ca65a61081cbdeaffe14ebb77a8fcf46839f
Paul Llanque <llanquepol@gmail.com>
2023-11-08 23:17:55 -0500

    [UPDATE] type and documentation added

--- src/components/baseComponents/numberButton/numberButton.component.tsx
+++ src/components/baseComponents/numberButton/numberButton.component.tsx
@@ -1,24 +1,35 @@
 interface NumberButtonProps {
   value: number;
-  roundedRight?: boolean;
-  roundedLeft?: boolean;
-  currentValue?: boolean;
+  isRoundedRight?: boolean;
+  isRoundedLeft?: boolean;
+  isCurrentValue?: boolean;
   getValue: (value: number) => void;
 }
 
+/**
++ * Renders a number button component.
++ *
++ * @param {NumberButtonProps} props - The props for the component.
++ * @param {number} props.value - The value of the button.
++ * @param {() => void} props.getValue - The function to call when the button is clicked.
++ * @param {boolean} props.roundedLeft - Whether the button has rounded left corners.
++ * @param {boolean} props.roundedRight - Whether the button has rounded right corners.
++ * @param {number} [props.currentValue=false] - The current value of the button.
++ * @return {React.ReactElement} The rendered number button.
++ */
 export const NumberButton: React.FC<NumberButtonProps> = ({
   value,
   getValue,
-  roundedLeft,
-  roundedRight,
-  currentValue = 0,
-}) => {
+  isRoundedLeft,
+  isRoundedRight,
+  isCurrentValue = false,
+}: NumberButtonProps): React.ReactElement => {
   return (
     <button
       className={`btn-number 
-                ${currentValue ? 'active' : 'border-l'}
-                ${roundedLeft && 'left-line'} 
-                ${roundedRight && 'right-line'}`}
+                ${isCurrentValue ? 'active' : 'border-l'}
+                ${isRoundedLeft ? 'left-line' : ''} 
+                ${isRoundedRight ? 'right-line' : ''}`}
       onClick={() => getValue(value)}>
       {value === 0 ? 'Any' : value + '+'}
     </button>
