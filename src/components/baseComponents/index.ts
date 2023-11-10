94deaa8df79eef987d09cd3549283040765070f3
Paul Llanque <llanquepol@gmail.com>
2023-11-10 10:13:48 -0500

    [ADD] card property atribuite added

--- src/components/baseComponents/index.ts
+++ src/components/baseComponents/index.ts
@@ -7,3 +7,4 @@ export { CheckBox } from './checkbox/checkbox.component';
 export { NumberButton } from './numberButton/numberButton.component';
 export { CardShip } from './cardShip/cardShip.component';
 export { CardButton } from './cardButton/cardButton.component';
+export { CardPropertyAtribuit } from './cardPropertyAtribuit/cardPropertyAtribuit.component';
