992595de07da481ec3b5954b17e94346960df463
Paul Llanque <llanquepol@gmail.com>
2023-11-10 15:24:40 -0500

    [FIX] import and invoque component fixed

--- src/components/compositeComponents/cardCompositeAtribuites/cardPropAtribuites.component.tsx
+++ src/components/compositeComponents/cardCompositeAtribuites/cardPropAtribuites.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BiBed, BiBath, BiArea } from 'react-icons/bi';
 import { MdPets } from 'react-icons/md';
 
-import { CardPropertyAtribuite } from '../../baseComponents';
+import { PropertyAtribuite } from '../../baseComponents';
 
 interface ICardPropAtribuitesProps {
   bed: number;
@@ -23,16 +23,14 @@ export const CardPropAtribuites: React.FC<ICardPropAtribuitesProps> = (
   const { bed, bath, area, pet } = props;
   return (
     <div className="card-composite__wrapper">
-      <CardPropertyAtribuite icon={<BiBed size={20} />} atribuite={bed} />
-      <CardPropertyAtribuite icon={<BiBath size={20} />} atribuite={bath} />
-      <CardPropertyAtribuite
+      <PropertyAtribuite icon={<BiBed size={20} />} atribuite={bed} />
+      <PropertyAtribuite icon={<BiBath size={20} />} atribuite={bath} />
+      <PropertyAtribuite
         icon={<BiArea size={20} />}
         atribuite={area}
         units="m2"
       />
-      {pet && (
-        <CardPropertyAtribuite icon={<MdPets size={20} />} atribuite={pet} />
-      )}
+      {pet && <PropertyAtribuite icon={<MdPets size={20} />} atribuite={pet} />}
     </div>
   );
 };
