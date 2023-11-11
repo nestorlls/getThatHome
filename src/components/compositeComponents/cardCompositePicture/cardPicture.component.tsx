7e5bab99fcadf867776522d53a54763067d6c822
Paul Llanque <llanquepol@gmail.com>
2023-11-10 23:10:14 -0500

    [ADD] cardPicture property composite component added

---
+++ src/components/compositeComponents/cardCompositePicture/cardPicture.component.tsx
@@ -0,0 +1,45 @@
+import { PROPERTY } from '@common/constant';
+import { CardShip, Picture } from '@components/baseComponents';
+import React from 'react';
+
+interface ICardPictureProps {
+  images: (string | undefined)[];
+  propertyName: string;
+  type: string;
+}
+
+export const CardPicture: React.FC<ICardPictureProps> = ({
+  images,
+  propertyName,
+  type,
+}): JSX.Element => {
+  const len = images.length;
+
+  return (
+    <div className="card-picture__wrapper">
+      {len > 0 ? (
+        <>
+          {images.map((image, index) => (
+            <div key={index} className="card-picture__composite">
+              <div className="card-picture__cardShip">
+                <CardShip type={type} />
+              </div>
+              <Picture image={image} alt={propertyName} size="p-card" />
+            </div>
+          ))}
+        </>
+      ) : (
+        <div className="card-picture__composite">
+          <div className="card-picture__cardShip">
+            <CardShip type={type} />
+          </div>
+          <Picture
+            image={PROPERTY.URL.PICTURE}
+            alt={propertyName}
+            size="p-card"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
--- src/components/compositeComponents/index.ts
+++ src/components/compositeComponents/index.ts
@@ -2,3 +2,4 @@ export { CompositeButton } from './compositeButton/compositeButton.component';
 export { CardFooter } from './cardFooter/cardFooter.component';
 export { CardPropAtribuites } from './cardCompositeAtribuites/cardPropAtribuites.component';
 export { CardPrice } from './cardPrice/cardPrice.component';
+export { CardPicture } from './cardCompositePicture/cardPicture.component';
