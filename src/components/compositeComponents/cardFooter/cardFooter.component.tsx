77cd62b96bf74385bad51d2b7dd295bae4b842f5
Paul Llanque <llanquepol@gmail.com>
2023-11-12 17:41:28 -0500

    [UPDATE] added docs in card footer component

--- src/components/compositeComponents/cardFooter/cardFooter.component.tsx
+++ src/components/compositeComponents/cardFooter/cardFooter.component.tsx
@@ -1,6 +1,13 @@
+import { FC, JSX } from 'react';
+
 import { CardButton } from '@components/baseComponents';
 
-export const CardFooter = () => {
+/**
+ * Renders the footer of a card property component.
+ *
+ * @returns JSX.Element - The rendered card footer.
+ */
+export const CardFooter: FC = (): JSX.Element => {
   return (
     <div className="card-footer__wrapper">
       <CardButton type="edit" />
