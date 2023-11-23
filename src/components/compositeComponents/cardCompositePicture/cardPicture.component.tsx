3b4bd07bfe171f9f07af4a7fe663a19cf092117a
Paul Llanque <llanquepol@gmail.com>
2023-11-23 08:54:58 -0500

    [UPDATE] pass only one picture when type is card and docs added

--- src/components/compositeComponents/cardCompositePicture/cardPicture.component.tsx
+++ src/components/compositeComponents/cardCompositePicture/cardPicture.component.tsx
@@ -8,6 +8,15 @@ interface ICardPictureProps {
   type: string;
 }
 
+/**
+ * Renders a card picture component.
+ *
+ * @param {ICardPictureProps} props - The properties for the card picture component.
+ * @param {Array} props.images - The array of images.
+ * @param {string} props.propertyName - The name of the property.
+ * @param {string} props.type - The type of the card picture.
+ * @returns {JSX.Element} The JSX element representing the card picture component.
+ */
 export const CardPicture: React.FC<ICardPictureProps> = ({
   images,
   propertyName,
@@ -18,16 +27,12 @@ export const CardPicture: React.FC<ICardPictureProps> = ({
   return (
     <div className="card-picture__wrapper">
       {len > 0 ? (
-        <>
-          {images.map((image, index) => (
-            <div key={index} className="card-picture__composite">
-              <div className="card-picture__cardShip">
-                <CardShip type={type} />
-              </div>
-              <Picture image={image} alt={propertyName} size="p-card" />
-            </div>
-          ))}
-        </>
+        <div className="card-picture__composite">
+          <div className="card-picture__cardShip">
+            <CardShip type={type} />
+          </div>
+          <Picture image={images[0]} alt={propertyName} size="p-card" />
+        </div>
       ) : (
         <div className="card-picture__composite">
           <div className="card-picture__cardShip">
