92811717dfa2dbba78eaa5a3f373130514022146
Paul Llanque <llanquepol@gmail.com>
2023-11-23 18:58:44 -0500

    [DOCS] documentation added

--- src/components/baseComponents/buttton/button.component.tsx
+++ src/components/baseComponents/buttton/button.component.tsx
@@ -8,6 +8,18 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   rightIcon?: React.ReactNode;
 }
 
+/**
+ * Renders a button component with customizable properties.
+ *
+ * @param {React.FC<ButtonProps>} ButtonProps - The props for the Button component.
+ * @param {React.ReactNode} children - The child elements of the Button.
+ * @param {string} typeBtn - The type of button (default: 'primary').
+ * @param {string} size - The size of the button (default: 'medium').
+ * @param {React.ReactNode} leftIcon - The left icon element of the button.
+ * @param {React.ReactNode} rightIcon - The right icon element of the button.
+ * @param {...any} props - Additional props for the button element.
+ * @returns {React.ReactNode} The rendered button component.
+ */
 export const Button: React.FC<ButtonProps> = ({
   children,
   typeBtn = 'primary',
