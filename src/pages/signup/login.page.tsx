31d33befde60dbc23e2f4d5939f25a91fd6c2fe3
Paul Llanque <llanquepol@gmail.com>
2024-02-10 12:16:04 -0500

    [ADD] login page

---
+++ src/pages/signup/login.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SignUpLayout } from '@layout/index';
+import { useParams } from 'react-router-dom';
+
+export const SignUpSeeker: React.FC = (): React.JSX.Element => {
+  const role = useParams();
+
+  console.log(role);
+
+  return <SignUpLayout>hola</SignUpLayout>;
+};
